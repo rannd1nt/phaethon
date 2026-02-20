cb25a604c440626d8bdfb596deafa34956b7a321
rannd1nt <vleaxorzz@gmail.com>
2026-02-21 02:55:08 +0700

    move unitflex to legacy

--- unitflex/__init__.py
+++ unitflex/__init__.py
@@ -1,6 +1,6 @@
 from .length import LengthConverter as length
 from .mass import MassConverter as mass
-from .temperature import TemperatureConverter as temperature
+from .temperature import temperatureConverter as temperature
 from .data import DataConverter as data
 from .volume import VolumeConverter as volume
 from .pressure import PressureConverter as pressure
@@ -11,4 +11,4 @@ __version__ = "0.9.3"
 __author__ = "Zahraan Dzakii"
 __license__ = "MIT"
 __description__ = "An Accurate and Flexible Unit Converter Library."
-__repository__ = "https://github.com/zranndts/unitflex"
+__repository__ = "https://github.com/rannd1nt/unitflex"
